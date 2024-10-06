0fef83fb82fe39a04d9901253ce854b3dfb1275c
emilakerman <emilaakerman@gmail.com>
2024-10-06 11:39:37 +0200

    made abbrevation for high m+ taps

--- lib/main.dart
+++ lib/main.dart
@@ -189,29 +189,123 @@ class PointsWidget extends StatefulWidget {
   State<PointsWidget> createState() => _PointsWidgetState();
 }
 
+String pointsFixer(int points) {
+  String fixedPoints = "";
+  switch (points) {
+    case > 1000000 && < 2000000:
+      fixedPoints = "+1M";
+      break;
+    case > 2000000 && < 3000000:
+      fixedPoints = "+2M";
+      break;
+    case > 3000000 && < 4000000:
+      fixedPoints = "+3M";
+      break;
+    case > 4000000 && < 5000000:
+      fixedPoints = "+4M";
+      break;
+    case > 5000000 && < 6000000:
+      fixedPoints = "+5M";
+      break;
+    case > 6000000 && < 7000000:
+      fixedPoints = "+6M";
+      break;
+    case > 7000000 && < 8000000:
+      fixedPoints = "+7M";
+      break;
+    case > 8000000 && < 9000000:
+      fixedPoints = "+8M";
+      break;
+    case > 9000000 && < 10000000:
+      fixedPoints = "+9M";
+      break;
+    case > 10000000 && < 20000000:
+      fixedPoints = "+10M";
+      break;
+    case > 20000000 && < 30000000:
+      fixedPoints = "+20M";
+      break;
+    case > 30000000 && < 40000000:
+      fixedPoints = "+30M";
+      break;
+    case > 40000000 && < 50000000:
+      fixedPoints = "+40M";
+      break;
+    case > 50000000 && < 60000000:
+      fixedPoints = "+50M";
+      break;
+    case > 60000000 && < 70000000:
+      fixedPoints = "+60M";
+      break;
+    case > 70000000 && < 80000000:
+      fixedPoints = "+70M";
+      break;
+    case > 80000000 && < 90000000:
+      fixedPoints = "+80M";
+      break;
+    case > 90000000 && < 100000000:
+      fixedPoints = "+90M";
+      break;
+    case > 100000000:
+      fixedPoints = "+100M";
+      break;
+    default:
+      fixedPoints = points.toString();
+  }
+
+  return fixedPoints.toString();
+}
+
 class _PointsWidgetState extends State<PointsWidget> {
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Text(
-          widget.points.toString(),
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            foreground: Paint()
-              ..style = PaintingStyle.stroke
-              ..strokeWidth = 6
-              ..color = Colors.black,
-          ),
+        Column(
+          children: [
+            Text(
+              pointsFixer(widget.points!),
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 6
+                  ..color = Colors.black,
+              ),
+            ),
+            Text(
+              widget.points.toString(),
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 4
+                  ..color = Colors.black,
+              ),
+            ),
+          ],
         ),
-        Text(
-          widget.points.toString(),
-          style: const TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 40,
-          ),
+        Column(
+          children: [
+            Text(
+              pointsFixer(widget.points!),
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),
+            ),
+            Text(
+              widget.points.toString(),
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ],
     );
